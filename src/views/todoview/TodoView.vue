cbc1f0d4c7dd4097542b533c09875a52a88cec7b
Kelvin Ward <kelvinward1010@gmail.com>
2024-11-24 17:14:59 +0700

    update modal

--- src/views/todoview/TodoView.vue
+++ src/views/todoview/TodoView.vue
@@ -1,41 +1,59 @@
 <script setup lang="ts">
-import FormWorkInList from './components/FormWorkInList.vue';
+import { ref } from "vue";
+import type { ButtonInstance } from "element-plus";
+import FormWorkInList from "./components/FormWorkInList.vue";
 
 const workList = [
-  { id: 0, lable: 'Work 1' },
-  { id: 1, lable: 'Work 2' },
-  { id: 2, lable: 'Work 3' }
-]
+    { id: 0, lable: "Work 1" },
+    { id: 1, lable: "Work 2" },
+    { id: 2, lable: "Work 3" },
+];
 
+const centerDialogVisible = ref(false);
 </script>
 
 <template>
-  <div class="container">
-    <div class="header">
-        <router-link class="linktoapp" to="/">
-            <h2>Todo Application</h2>
-        </router-link>
-    </div>
+    <div class="container">
+        <div class="header">
+            <router-link class="linktoapp" to="/">
+                <h2>Todo Application</h2>
+            </router-link>
+        </div>
 
-    <div class="content">
-        <el-scrollbar height="400px">
-            <FormWorkInList 
-                v-for="item in workList"         
-                :work="item"
-                :key="item.id"
-            />
-        </el-scrollbar>
+        <div class="content">
+            <div class="actions">
+                <div class="create">
+                    <el-button plain @click="centerDialogVisible = true">
+                        Create
+                    </el-button>
+                    <el-dialog v-model="centerDialogVisible" title="Warning" width="500" align-center>
+                        <span>Open the dialog from the center from the screen</span>
+                        <template #footer>
+                            <div class="dialog-footer">
+                                <el-button @click="centerDialogVisible = false">Cancel</el-button>
+                                <el-button type="primary" @click="centerDialogVisible = false">
+                                    Confirm
+                                </el-button>
+                            </div>
+                        </template>
+                    </el-dialog>
+                </div>
+            </div>
+            <el-scrollbar height="400px">
+                <FormWorkInList v-for="item in workList" :work="item" :key="item.id" />
+            </el-scrollbar>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
-.container{
+.container {
     width: 100vw;
     height: 100vh;
     overflow: hidden;
 }
-.header{
+
+.header {
     width: 100vw;
     padding: 10px;
     text-align: center;
@@ -43,12 +61,25 @@ const workList = [
     color: white;
 }
 
-.header h2{
+.header h2 {
     color: white;
 }
 
-.header .linktoapp{
+.header .linktoapp {
     text-decoration: none;
 }
 
+.actions{
+    padding: 20px;
+}
+
+.actions .create{
+    width: fit-content;
+}
+
+.content .actions {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+}
 </style>
